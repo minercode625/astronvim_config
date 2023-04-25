23164c01c9b66ec5697fe197ca8db04be596b692
minercode625 <seowangduk@gmail.com>
2023-04-25 23:34:36 +0900

    My Init Page

--- plugins/core.lua
+++ plugins/core.lua
@@ -5,17 +5,35 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+
+        "__          __    _                                _                       ",
+        "\\ \\        / /   | |                              | |                      ",
+        " \\ \\  /\\  / /___ | |  ___  ___   _ __ ___    ___  | |                      ",
+        "  \\ \\/  \\/ // _ \\| | / __|/ _ \\ | '_ ` _ \\  / _ \\ | |                      ",
+        "   \\  /\\  /|  __/| || (__| (_) || | | | | ||  __/ |_|                      ",
+        "    \\/  \\/  \\___||_| \\___|\\___/ |_| |_| |_| \\___| (_)                      ",
+        "__          __                   _____          _       _____              ",
+        "\\ \\        / /                  |  __ \\        | |     / ____|             ",
+        " \\ \\  /\\  / /__ _  _ __    __ _ | |  | | _   _ | | __ | (___    ___   ___  ",
+        "  \\ \\/  \\/ // _` || '_ \\  / _` || |  | || | | || |/ /  \\___ \\  / _ \\ / _ \\ ",
+        "   \\  /\\  /| (_| || | | || (_| || |__| || |_| ||   <   ____) ||  __/| (_) |",
+        "    \\/  \\/  \\__,_||_| |_| \\__, ||_____/  \\__,_||_|\\_\\ |_____/  \\___| \\___/ ",
+        "                           __/ |                                           ",
+        "                          |___/                                            ",
+
+        --
+        -- " █████  ███████ ████████ ██████   ██████",
+        -- "██   ██ ██         ██    ██   ██ ██    ██",
+        -- "███████ ███████    ██    ██████  ██    ██",
+        -- "██   ██      ██    ██    ██   ██ ██    ██",
+        -- "██   ██ ███████    ██    ██   ██  ██████",
+        -- " ",
+        -- "    ███    ██ ██    ██ ██ ███    ███",
+        -- "    ████   ██ ██    ██ ██ ████  ████",
+        -- "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+        -- "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+        -- "    ██   ████   ████   ██ ██      ██",
+        -- "     Welcome! Wangduk. Let's Work",
       }
       return opts
     end,
@@ -27,7 +45,7 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
@@ -36,7 +54,7 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
@@ -44,18 +62,18 @@ return {
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          -- don't add a pair if the next character is %
+              :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+              :with_pair(
+                cond.not_before_regex("xxx", 3)
+              )
+          -- don't move right when repeat character
+              :with_move(cond.none())
+          -- don't delete if the next character is xx
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_cr(cond.none()),
         },
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
