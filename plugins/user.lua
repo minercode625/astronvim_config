7e7e2f2c8976e0bebad92d8c09915279402370e3
minercode625 <seowangduk@gmail.com>
2024-01-26 23:28:36 +0900

    Markdown and Obsidian

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,12 +8,6 @@ return {
     event = "VeryLazy",
   },
   {
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    config = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    ft = { "markdown" },
-  },
-  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "User AstroFile",
